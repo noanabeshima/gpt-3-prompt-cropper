c2ab5a4a040248aefe3e1fc5311ea355caf1d4c9
Noa Nabeshima <noanabeshima@gmail.com>
2020-08-13 19:17:45 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from encoder import get_encoder
+import os
+import pyperclip
+
+model_dir = os.path.join(os.getcwd(), 'models')
+enc = get_encoder('124M', model_dir)
+
+def crop_prompt(prompt: str):
+    global enc
+
+    cropped_prompt = enc.decode(enc.encode(prompt)[:2048])
+    return cropped_prompt
+
+def main():
+    prompt = crop_prompt(pyperclip.paste())
+    pyperclip.copy(''.join(prompt))
+
+if name == '__main__':
+    main()
